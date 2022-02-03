e9e1c1e6c550827aab79e4370ed0b16437da05ad
Tofu Green <thegreentofu@rocketmail.com>
2022-02-03 01:03:33 +0000

    ghfhtdft

--- RougelikeGame/MapGenerator.cs
+++ RougelikeGame/MapGenerator.cs
@@ -6,5 +6,36 @@ namespace RougelikeGame
 {
     class MapGenerator
     {
+        public Room startingRoom;
+        public Room endRoom;
+
+        public List<Room> myRooms;
+        public int roomCount = 10;
+
+        public MapGenerator()
+        {
+            //startingRoom = new Room();
+            //startingRoom.RandomiseRoom("startRoom" ,5, 11, 5, 11);
+            //endRoom = new Room();
+            //endRoom.RandomiseRoom("endRoom", 5, 11, 5, 11);
+
+            myRooms = new List<Room>();
+
+            RoomInit();
+        }
+
+        public void RoomInit()
+        {
+            for (int i = 0; i < roomCount; i++)
+            {
+                //Create a temp room
+                Room newRoom = new Room();
+                newRoom.RandomiseRoom("R" + i, 5, 11, 5, 11); //customise it
+
+                //add it to my collection
+                myRooms.Add(newRoom);
+            }
+        }
+
     }
 }
--- RougelikeGame/Program.cs
+++ RougelikeGame/Program.cs
@@ -4,9 +4,20 @@ namespace RougelikeGame
 {
     class Program
     {
+        static MapGenerator roomManager;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello World!");
+            //master test 2 now doing the stream to show this off branching test
+            roomManager = new MapGenerator();
+            //roomManager.RoomInit();
+        }
+
+        public void SpecialFunctionality()
+        {
+            //very awesome code
         }
     }
 }
